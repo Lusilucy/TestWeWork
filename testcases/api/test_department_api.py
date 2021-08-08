d2581ff6dc2838f3fb3cfa74b220c2eaf88b2580
lusilucy <421445227@qq.com>
2021-08-09 02:48:18 +0800

    更新环境配置

--- testcases/api/test_department_api.py
+++ testcases/api/test_department_api.py
@@ -2,7 +2,9 @@
 import sys
 import os
 curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.abspath(os.path.dirname(curPath) + os.path.sep + ".")
+print(curPath)
+rootPath = os.path.abspath(os.path.dirname(curPath) + os.path.sep + "..")
+print(rootPath)
 sys.path.append(rootPath)
 import allure
 import pytest
