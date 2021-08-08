c2e3e8d8188cc8c2ca4f13f133e33e50860fd838
lusilucy <421445227@qq.com>
2021-08-09 02:13:54 +0800

    更新环境配置

--- testcases/api/test_department_api.py
+++ testcases/api/test_department_api.py
@@ -1,6 +1,7 @@
 # 接口测试案例-部门
 import sys
 sys.path.append("/Users/lusi/.jenkins/workspace/TestWeWork_API/TestWeWork")
+# sys.path.append("/Users/lusi/PycharmProjects/TestWeWork")
 import allure
 import pytest
 
